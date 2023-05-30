413b92e5593fb795a1500f92cc72883fa897feca
Daniela-Prada <daniela.prada@skyline.be>
2023-05-30 13:54:31 -0500

    Add project files.

---
+++ EmpowerHelloWorld_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHelloWorld_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHelloWorld_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4FB6A580-05AB-4B55-B940-7B663C0A8E73")]
+[assembly: AssemblyVersion("1.0.0.0")]
